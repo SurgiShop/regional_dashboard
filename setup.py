211cc3e546881e59ee70c18e521fa986d376ede6
garystarr-surgi <gary.starr@surgishop.com>
2025-12-04 13:41:58 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='regional_dashboard',
+    version='0.0.1',
+    description='Regional Dashboard',
+    author='SurgiShop',
+    author_email='gary.starr@surgishop.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=['frappe']
+)
